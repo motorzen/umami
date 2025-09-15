cd6d2638fb93fc96bbf0af7eae839980cd9ff543
motorzen <motorzen@gmail.com>
2025-09-15 10:53:40 -0600

    Update build-geo.js

--- scripts/build-geo.js
+++ scripts/build-geo.js
@@ -1,35 +1,20 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-console */
 const fs = require('fs');
 const path = require('path');
 
-// Detect Vercel environment (optional)
-if (process.env.VERCEL) {
-  console.log('Vercel environment detected. Skipping geo setup.');
-  process.exit(0);
-}
-
-// Destination folder for Umami to read GeoIP database from
-const dest = path.resolve(__dirname, '../geo');
+const src = path.join(__dirname, '../geoip/GeoLite2-City.mmdb');
+const destDir = path.join(__dirname, '../geo');
 
 // Ensure destination folder exists
-if (!fs.existsSync(dest)) {
-  fs.mkdirSync(dest, { recursive: true });
+if (!fs.existsSync(destDir)) {
+  fs.mkdirSync(destDir, { recursive: true });
 }
 
-// Path to the local GeoIP database in your repo
-const localGeoDb = path.resolve(__dirname, '../geoip/GeoLite2-City.mmdb');
+// Copy database to a folder that will be included in the build
+const dest = path.join(destDir, 'GeoLite2-City.mmdb');
+fs.copyFileSync(src, dest);
+console.log('GeoIP database copied for runtime use:', dest);
 
-if (!fs.existsSync(localGeoDb)) {
-  console.error('Local GeoIP database not found at:', localGeoDb);
-  process.exit(1);
-}
-
-// Copy the local database to the expected folder
-const target = path.join(dest, 'GeoLite2-City.mmdb');
-fs.copyFileSync(localGeoDb, target);
-
-console.log('Using local GeoIP database:', target);
-
-// Exit successfully
-process.exit(0);
+// Set environment variable for Umami (used at runtime)
+process.env.GEOIP_DB = dest;
+console.log('GEOIP_DB set to:', process.env.GEOIP_DB);
